45fc7ddea83d3e4f19f5d0e6a965eee73df4dc47
A20Pablomd <a20pablomd@iessanclemente.net>
2025-03-24 18:50:37 +0100

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,1 +1,19 @@
+# test_utils.py
+import unittest
+from utils import StringUtils
 
+class TestStringUtils(unittest.TestCase):
+    def test_reverse_string(self):
+        self.assertEqual(StringUtils.reverse_string("hello"), "olleh")
+        self.assertEqual(StringUtils.reverse_string("123"), "321")
+
+    def test_capitalize_string(self):
+        self.assertEqual(StringUtils.capitalize_string("hello"), "HELLO")
+        self.assertEqual(StringUtils.capitalize_string("HeLlO"), "HELLO")
+
+    def test_lowercase_string(self):
+        self.assertEqual(StringUtils.lowercase_string("HELLO"), "hello")
+        self.assertEqual(StringUtils.lowercase_string("HeLlO"), "hello")
+
+if __name__ == '__main__':
+    unittest.main()
