6a797419d1dbf6e3f27bd1a22eaf25e3a7e96491
A20Pablomd <a20pablomd@iessanclemente.net>
2025-03-24 18:49:59 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,16 @@
+# utils.py
+class StringUtils:
+    @staticmethod
+    def reverse_string(string):
+        """Reverse a given string"""
+        return string[::-1]
 
+    @staticmethod
+    def capitalize_string(string):
+        """Capitalize all letters in a string"""
+        return string.upper()
+
+    @staticmethod
+    def lowercase_string(string):
+        """Convert all letters in a string to lowercase"""
+        return string.lower()
